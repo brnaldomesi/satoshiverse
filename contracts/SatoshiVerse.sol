4105290a1da297fe619cb0e310aaae3fe1d270fc
lo <innovations@herasoft.com>
2021-11-19 13:42:42 -0500

    add function comments

--- contracts/SatoshiVerse.sol
+++ contracts/SatoshiVerse.sol
@@ -50,26 +50,22 @@
   // ***************//((/*(*///********************///((##%%&&&&&&&&&&%%%####(#((##%%%%%%%%%%%%%%%&#&&&&///////////////*                                          
   // ****************,((//#*/*#******************////////////((#(#((#%#&%####((#%%%%%%%%%%%%%%&%%%&&%&&****************,                                          
   // //////////////////((((((*(/,(*********************************//////((((#(######%%%%%%&#%%%&&&&&%(((((((((((((((((*                                          
-  // ///////////////////##(((#*((#,#***********************/,***(***/////////(((((##%%%%%&%&&%%&&&&&#******************,                                          
-  // /////////////////////##((#/#(((,#**************(/,,*,/,//((/,#**,/%(////(((((%#%%%%%&&&%%&&&&&////////////////////,                                          
-  // ///////////////////////####*(/(//(,#*/*###/,,,/#,%(,#&&&&&#&&@*(*(#%//*/%%&&(#%&#&%&&&&%&&&&//////////////////////,                                          
-  // ///////////////////////###%#*((/(////,,*##/,*/(((,(%%@*,,,**&@*%((/(((#(%&&#((&%&%%%&&&&&(((//////////////////////,                                          
-  // ///////////////////////##(//%(%((///((/(/**(/*(#,,#@*//***/*(##(@/(/((/(#&#&&%&%%%&&&&#//(((//////////////////////,                                          
-  // ///////////////////////##(((((%%#(#(((/*(((*@*&,/(@@#(****//((@@@/%(#%&#%#&%%&&&&&&&(/////((//////////////////////,                                          
-  // ////////////////////////////////(&%####%(&/&,@@@@@(%/,/(####*(&(@@@@@@&&&&&&&&&&&&**********//////////////////////,                                          
-  // ////////////////////////////////(#((@&&&&(%*%@@@@@#(%%%#**#%%&%@@@@@@@&&&@&@&&&(##/*********//////////////////////,                                          
-  // ////////////////////////////////(#(((#(#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%#((###/*********//////////////////////,                                          
-  // ////////////////////////////////(#(((#(###%#%%&@@@@@@@@@@@@@@@@@@@@&&##%%##%#((###/*********//////////////////////,   
+  // ///////////////////##(((#*((#,#***********************/,***(***/////////(((((##%%%%%&%&&%%&&&&&#*,                                          
+  // /////////////////////##((#/#(((,#**************(/,,*,/,//((/,#**,/%(////(((((%#%%%%%&&&%%&&&&&/,                                          
+  // ///////////////////////####*(/(//(,#*/*###/,,,/#,%(,#&&&&&#&&@*(*(#%//*/%%&&(#%&#&%&&&&%&&&&/,                                          
+  // ///////////////////////###%#*((/(////,,*##/,*/(((,(%%@*,,,**&@*%((/(((#(%&&#((&%&%%%&&&&&(((,                                          
+  // ///////////////////////##(//%(%((///((/(/**(/*(#,,#@*//***/*(##(@/(/((/(#&#&&%&%%%&&&&#//(((,                                          
+  // ///////////////////////##(((((%%#(#(((/*(((*@*&,/(@@#(****//((@@@/%(#%&#%#&%%&&&&&&&(/////((,                                          
+  // ////////////////////////////////(&%####%(&/&,@@@@@(%/,/(####*(&(@@@@@@&&&&&&&&&&&&**********,                                          
+  // ////////////////////////////////(#((@&&&&(%*%@@@@@#(%%%#**#%%&%@@@@@@@&&&@&@&&&(##/*********,                                          
+  // ////////////////////////////////(#(((#(#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%#((###/*********,                                          
+  // ////////////////////////////////(#(((#(###%#%%&@@@@@@@@@@@@@@@@@@@@&&##%%##%#((###/*********,   
 
 
 
 /*
-* Gas optimization 
-*
-*
-*
-*
-*
+*Welcome to the Satoshiverse! The Satoshiverse is an epic comic NFT collaboration between Apollo NFT Studios, Jose Delbo, and YOU, the NFT community. 
+*It tells the story of our hero, Satoshi The Creator and his quest to save the world from the Defenders of Fiat and the many other foes who lie ahead.
 */ 
 pragma solidity ^0.8.0;
 
@@ -81,11 +77,14 @@ import "./helpers/NumberHelper.sol";
 
 
  /**
-    * 
+    * SatoshiVerse Legionnaire Avatar NFT Contract V1 
+    * Provided by Satoshiverse LLC
+    * Authored by brnaldomesi a Senior Solidity Developer @ Herasoft
     */
 contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
   ILegionnaire public legionnaire;
 
+  // Payable Address for the Initial Sale
   address payable public svEthAddr = payable(0x981268bF660454e24DBEa9020D57C2504a538C57);
   
   uint16 _claimSV = 1;
@@ -93,7 +92,7 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
 
   uint256 SV_MAX = 10000;
   
-  uint256 _activeDateTime = 1637683200; // November 23th at 11:00 AM EST
+  uint256 _activeDateTime; 
   uint256 INTERVAL = 3600;
   uint256 randNonce;
   
@@ -112,8 +111,12 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
   string[] leftoverUris;
 
   mapping(address => mapping(string => uint8)) public tokensCount;
+
+  // Only through Day 4 Mints
   mapping(address => uint8) public purchasedSoFar;
   
+
+  // Set Initial Addresses and Variables Upon Deployment
   constructor(
     address _operator,
     address _uriSetter,
@@ -134,16 +137,13 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
     addURISetter(_uriSetter);
   }
 
-    /**
-    * 
-    */
-
-  function setPaymentAddress(address _svEthAddr) external onlyURISetter {
+// Change the Payment Adddress if Necessary
+  function setPaymentAddress(address _svEthAddr) external onlyOwner {
     svEthAddr = payable(_svEthAddr);
   }
- /**
-    * 
-    */
+
+ // Operator Account Sets the Contract with a List of Presale Holders Addresses
+ // Snapshot was taken 12pm EST Monday November 22, 2021
   function seedPresaleWhiteList(address[] calldata users, string calldata tokenType, uint8[] calldata counts) external onlyOperator {
     require(users.length == counts.length, "Mismatched presale addresses and counts");
 
@@ -152,25 +152,18 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
     }
   }
 
-/**
-*
-*/
+// Operator can toggle the claim mechanism as On / Off
   function toggleClaim() external onlyOperator {
     claimState = !claimState;
   }
-
+// Operator can toggle the purchasing mechanism as On / Off for the Sale of Legionnaires
   function togglePurchase() external onlyOperator {
     purchaseState = !purchaseState;
   }
 
-
-/* TODO 
-* Make Public 
-*
-*
-*
-*/
+// Returns a Random Legionnaire from the set of Random Legionniares 
   function popRandomTokenURI() internal returns(string memory) {
+    // legtOverUris === unpurchased / unclaimed Legionnaires 
     uint256 randomIndex = getRandomIndex(leftoverUris.length);
     string memory tokenURI = leftoverUris[randomIndex];
     leftoverUris[randomIndex] = leftoverUris[leftoverUris.length - 1];
@@ -178,12 +171,7 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
     return tokenURI;
   }
 
-    /**
-    * User Claims their Legionnaire based on their holding
-    * TODOs
-    * 
-    *
-    */
+  // A secure function for Claiming Legionnaires on a specific window of time dependant on the presale token the user has. 
   function claim(uint256 claimedCount) external nonReentrant {
     require(claimState, "Claim is disabled");
     require(block.timestamp >= _activeDateTime, "Presale not start yet");
@@ -235,11 +223,8 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
     tokensCount[msg.sender]['silver'] = silverTokenCount;
   }
 
-   /**
-    * 
-    * 
-    */
-
+  
+// A secure function to purchase a Legionnaire 
   function purchase(uint256 count) external payable nonReentrant {
     require(purchaseState, "Purchase is disabled");
     require(block.timestamp >= _activeDateTime, "Sale not start yet");
@@ -252,6 +237,7 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
       limit = purchasedSoFar[msg.sender];
       require(count + limit > 0 && count + limit < 3, "Not allowed to purchase that amount");
       purchasedSoFar[msg.sender] += uint8(count);
+     // on Day 5 you can only purhcase up to 10 / transaction 
     } else if (passedDays < 6) {
       require(count < 11, "Up to 10 only");
     }
@@ -288,17 +274,9 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
     }
   }
 
-   /**
-    * 
-    */
+   // Operator can set the start time in UNIX stamp for the claim and sale period 
   function setActiveDateTime(uint256 activeDateTime) external onlyOperator {
     _activeDateTime = activeDateTime;
-  }
-   /**
-    * 
-    */
-  function setInterval(uint256 interval) external onlyOperator {
-    INTERVAL = interval;
   }
 
 /** TODO
@@ -319,13 +297,15 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
       revealState = true;
     }
   }
-   /**
-    * 
-    */
+   
+   // A Random Index between zero and range leveraging VRF 
+  
   function getRandomIndex(uint256 range) internal returns(uint256) {
     randNonce++;
     return uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce, randomNess))) % range;
   }
+
+// Operator can batch mint and trasnfer remaining Legionnaires to a secure address
 
   function safeBatchMintAndTransfer(address holder, bool isSetUri, uint16 batchSize) external onlyOperator {
     require(revealState, "Have to begin Self-Reveal");
@@ -340,9 +320,8 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
 
     _purchaseSV = uint16(_purchaseSV + batchSize);
   }
- /**
-    * 
-    */
+ // URISetter will call this to randomly pair URIs with NFT Metadata to tokens.
+
   function pairLegionnairesWithUris(uint16[] memory _tokenIds, string[] memory _tokenURIs) external onlyURISetter {
     require(_tokenIds.length == _tokenURIs.length, "Mismatched ids and URIs");
     require(_tokenIds.length > 0, "Empty parameters");
@@ -359,24 +338,19 @@ contract SatoshiVerse is VRFConsumerBase, Operatorable, ReentrancyGuard {
       assembly { mstore(_tokenURIs, sub(mload(_tokenURIs), 1)) }
     }
   }
- /**
-    * 
-    */
+
+ // Function saves the random nonce from VRF into the contract 
   function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
     if(requestId == _requestId) {
       randomNess = _randomness;
     }
   }
- /**
-    * 
-    */
+ // Owner can decrease the total supply not ever exceeding 10,000 Legionnaires
   function setMaxLimit(uint256 maxLimit) external onlyOwner {
     require(maxLimit < 10001, "Exceed max limit 10000");
     SV_MAX = maxLimit;
   }
- /**
-    * 
-    */
+ // Operator Calls to VRF for a random nonce
   function requestRandomToVRF() external onlyOperator {
     require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
     requestId = requestRandomness(keyHash, fee);
